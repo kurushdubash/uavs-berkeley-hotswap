540ce6cc3f035f4e900d9d0c3206605c5b628fd5
Kurush Dubash <dubash.kurush@berkeley.edu>
2016-10-10 13:08:02 -0700

    Added option to left click and see convex hull

--- block.py
+++ block.py
@@ -5,13 +5,16 @@ cam = SimpleCV.Camera(0)
 # address = "admin:admin@192.168.29.206:8081/video"
 # cam = SimpleCV.JpegStreamCamera(address)
 normaldisplay = True
+hull = False
 MIN_AREA = 2500
 while display.isNotDone():
 
 	if display.mouseRight:
 		normaldisplay = not(normaldisplay)
 		print "Display Mode:", "Normal" if normaldisplay else "Segmented" 
-	
+	if display.mouseLeft:
+		hull = not(hull)
+
 	img = cam.getImage().flipHorizontal()
 	dist = img.hueDistance(SimpleCV.Color.BLACK).dilate(2).invert()
 	segmented = dist.stretch(235,255)
@@ -27,15 +30,16 @@ while display.isNotDone():
 				y = largest_square.y - (height /2) 
 
 				# Use for convex hull instead of rectangle
-				# list_of_points = largest_square.hull()
-				# tempx = list_of_points[0][0]
-				# tempy = list_of_points[0][1]
-				# for x1,y1 in list_of_points:
-				# 	img.drawLine((tempx,tempy), (x1,y1), SimpleCV.Color.BLUE, 3)
-				# 	tempx=x1
-				# 	tempy=y1
-
-				img.drawRectangle(x, y, width, height, SimpleCV.Color.BLUE, 3)
+				if hull:
+					list_of_points = largest_square.hull()
+					tempx = list_of_points[0][0]
+					tempy = list_of_points[0][1]
+					for x1,y1 in list_of_points:
+						img.drawLine((tempx,tempy), (x1,y1), SimpleCV.Color.CYAN, 3)
+						tempx=x1
+						tempy=y1
+				else:
+					img.drawRectangle(x, y, width, height, SimpleCV.Color.BLUE, 3)
 				img.drawCircle((largest_square.x,largest_square.y), 3, SimpleCV.Color.BLUE, 3)
 	if normaldisplay:
 		img.show()

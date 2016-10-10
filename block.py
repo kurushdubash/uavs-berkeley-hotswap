faf4d24597b50bff022bc057e9e928d1f1ea0d1b
Kurush Dubash <dubash.kurush@berkeley.edu>
2016-10-10 13:00:38 -0700

    Added commented code for convext hull instead of rectangle

--- block.py
+++ block.py
@@ -13,11 +13,11 @@ while display.isNotDone():
 		print "Display Mode:", "Normal" if normaldisplay else "Segmented" 
 	
 	img = cam.getImage().flipHorizontal()
-	dist = img.hueDistance(SimpleCV.Color.BLACK).dilate(1).invert()
+	dist = img.hueDistance(SimpleCV.Color.BLACK).dilate(2).invert()
 	segmented = dist.stretch(235,255)
 	blobs = segmented.findBlobs()
 	if blobs:
-		squares = blobs.filter([b.isRectangle(0.2) for b in blobs])
+		squares = blobs.filter([b.isRectangle(0.15) for b in blobs])
 		if squares:
 			largest_square = squares[-1]
 			if largest_square.area() >= MIN_AREA:
@@ -25,11 +25,16 @@ while display.isNotDone():
 				height = largest_square.height()
 				x = largest_square.x - (width / 2)
 				y = largest_square.y - (height /2) 
-				# img.drawLine((x,y), (x,y+height), SimpleCV.Color.BLUE, 3)
-				# img.drawLine((x,y), (x+width,y), SimpleCV.Color.BLUE, 3)
-				# img.drawLine((x,y+height), (x+width,y+height), SimpleCV.Color.BLUE, 3)
-				# img.drawLine((x+width,y+height), (x+width,y), SimpleCV.Color.BLUE, 3)
-				# print largest_square.area()
+
+				# Use for convex hull instead of rectangle
+				# list_of_points = largest_square.hull()
+				# tempx = list_of_points[0][0]
+				# tempy = list_of_points[0][1]
+				# for x1,y1 in list_of_points:
+				# 	img.drawLine((tempx,tempy), (x1,y1), SimpleCV.Color.BLUE, 3)
+				# 	tempx=x1
+				# 	tempy=y1
+
 				img.drawRectangle(x, y, width, height, SimpleCV.Color.BLUE, 3)
 				img.drawCircle((largest_square.x,largest_square.y), 3, SimpleCV.Color.BLUE, 3)
 	if normaldisplay:

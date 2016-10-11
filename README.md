335ac05b6acb1beadda6429b28c76028ccd3b3d8
Kurush Dubash <dubash.kurush@berkeley.edu>
2016-10-10 19:03:16 -0700

    Added option for cyan circle but removed it, now highlights red square

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # uavs-berkeley-hotswap
+
+Requirement:
+- Pillow
+- SimpleCV
+- WebCam or IP Cam
 UAVs@Berkeley HotSwap Project https://uav.berkeley.edu/midair-hot-swap/
--- block.py
+++ block.py
@@ -22,20 +22,22 @@ while display.isNotDone():
 	only_red = img - away_from_red
 	segmented_red = only_red.erode(5).binarize().invert()
 	
-	away_from_cyan = img.colorDistance((0,255,255))
-	only_cyan = img - away_from_cyan
-	segmented_cyan = only_cyan.erode(5).binarize().invert()
+	# away_from_cyan = img.colorDistance((0,255,255))
+	# only_cyan = img - away_from_cyan
+	# segmented_cyan = only_cyan.erode(5).binarize().invert()
 	
 	# Option 2
 	# dist = img.hueDistance(SimpleCV.Color.BLACK).dilate(2).invert()
 	# segmented = dist.stretch(220,255)
 	square_found = False
+	biggest_blob = None
 	blobs = segmented_red.findBlobs()
 	if blobs:
 		squares = blobs.filter([b.isRectangle(0.13) for b in blobs])
 		if squares:
 			square_found = True
 			largest_square = squares[-1]
+			biggest_blob = largest_square
 			if largest_square.area() >= MIN_AREA:
 				width = largest_square.width()
 				height = largest_square.height()
@@ -54,9 +56,9 @@ while display.isNotDone():
 				else:
 					img.drawRectangle(x, y, width, height, SimpleCV.Color.BLUE, 3)
 				img.drawCircle((largest_square.x,largest_square.y), 3, SimpleCV.Color.BLUE, 3)
-	circles = segmented_cyan.findBlobs()
-	if circles and square_found:
-		circle = circles.filter([c.isCircle(0.13) for c in circles])
+	# circles = segmented_cyan.findBlobs()
+	# if circles and square_found:
+		# circle = circles.filter([c.isCircle(0.13) for c in circles])
 		# if circle:
 		# 	largest_circle = circle[-1]
 		# 	img.drawCircle((largest_circle.x,largest_circle.y), 3, SimpleCV.Color.BLUE, 3)
@@ -65,6 +67,8 @@ while display.isNotDone():
 	if normaldisplay == 2:
 		segmented_cyan.show()
 	elif normaldisplay == 1:
+		if biggest_blob != None:
+			biggest_blob.draw((255,0,0))
 		segmented_red.show()
 	else:
 		img.show()

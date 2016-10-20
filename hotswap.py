b7a627c4897fc1e5f85b1a962548940dbd930381
Kurush Dubash <dubash.kurush@berkeley.edu>
2016-10-20 16:26:13 -0700

    Cleaned up code, renamed method calls, added try catch for quiting

--- hotswap.py
+++ hotswap.py
@@ -11,6 +11,7 @@ def display_overlay(img, biggest_blob, normaldisplay, segmented_red):
 	""" Displays the camera image in the display. Also sets up 
 			mouse clicks to switch between display modes 
 	"""
+	img.drawCircle(CENTER, 3, SimpleCV.Color.BLUE, 3)
 	if normaldisplay == 1:
 		if biggest_blob != None:
 			biggest_blob.draw((255,0,0))
@@ -18,38 +19,52 @@ def display_overlay(img, biggest_blob, normaldisplay, segmented_red):
 	else:
 		img.show()
 
-def vector_to_center(img, x_y, color=(0,255,0), strength=5, varying_strength=True):
+def display_vector_to_center(img, x_y, color=(0,255,0), strength=5, varying_strength=True):
 	""" Draws a vector to the center of the screen to indicate how far from the 
 		center we are
 	"""
 	img.drawLine(x_y, CENTER, color, strength)
 
-def border(img, color=(0,255,0), strength=15):
+def display_border(img, color=(0,255,0), strength=15):
 	""" Draws a line around the bodrer of the screen """
 	img.drawRectangle(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT, color, strength)
 
-def abs_distance(tuple1, tuple2):
-	""" Returns the absolute difference between point of tuple1 and tuple2 """
-	return ((tuple2[0] - tuple1[0]) ** 2 + (tuple2[1] - tuple1[1]) ** 2) ** (0.5)
-
-def check_position(img, blob):	
-	""" If the center of our screen is in the largest blob, we turn the border green.
-		If not but we turn the screen red.
-	"""				
-	if blob.contains(CENTER):
-		border(img)
-	else:
-		border(img, (255,0,0))
-
-def draw_hull(img, blob):
+def display_hull(img, blob):
 	""" Draws a convex hull of the blob. """
 	list_of_points = blob.hull()
-	tempx = list_of_points[0][0]
-	tempy = list_of_points[0][1]
-	for x1,y1 in list_of_points:
-		img.drawLine((tempx,tempy), (x1,y1), SimpleCV.Color.CYAN, 3)
-		tempx=x1
-		tempy=y1
+	# tempx = list_of_points[0][0]
+	# tempy = list_of_points[0][1]
+	# for x1,y1 in list_of_points:
+	# 	img.drawLine((tempx,tempy), (x1,y1), SimpleCV.Color.CYAN, 3)
+	# 	tempx=x1
+	# 	tempy=y1
+	# Experimental code to get sharp rectangle that works with orientation
+	# SUPER inneffient right now
+	largest = 0
+	point1 = None
+	point2 = None
+	point3 = None
+	point4 = None
+	for x,y in list_of_points:
+		for a,b in list_of_points:
+			if abs_distance((x,y), (a,b)) > largest:
+				largest = abs_distance((x,y), (a,b))
+				point1 = (x,y)
+				point2 = (a,b)
+	list_of_points.remove(point1)
+	list_of_points.remove(point2)
+	largest = 0
+	for x,y in list_of_points:
+		for a,b in list_of_points:
+			if abs_distance((x,y), (a,b)) > largest:
+				largest = abs_distance((x,y), (a,b))
+				point3 = (x,y)
+				point4 = (a,b)
+	img.drawCircle(point3, 3, SimpleCV.Color.BLUE, 3)
+	img.drawCircle(point4, 3, SimpleCV.Color.BLUE, 3)
+	img.drawCircle(point1, 3, SimpleCV.Color.BLUE, 3)
+	img.drawCircle(point2, 3, SimpleCV.Color.BLUE, 3)
+
 
 def display_distance(img, blob=None):
 	""" Draws text on the img layer displaying the distance from the object. """ 
@@ -71,8 +86,21 @@ def display_orientation(img, blob=None):
 	text_layer.ezViewText(txt, (20,40), (0,0,0), (255,255,255))
 	img.addDrawingLayer(text_layer)
 
-def run(cam=SimpleCV.Camera(0)):
-	display = SimpleCV.Display()
+
+def abs_distance(tuple1, tuple2):
+	""" Returns the absolute difference between point of tuple1 and tuple2 """
+	return ((tuple2[0] - tuple1[0]) ** 2 + (tuple2[1] - tuple1[1]) ** 2) ** (0.5)
+
+def check_position(img, blob):	
+	""" If the center of our screen is in the largest blob, we turn the border green.
+		If not but we turn the screen red.
+	"""				
+	if blob.contains(CENTER):
+		display_border(img)
+	else:
+		display_border(img, (255,0,0))
+
+def run(cam=SimpleCV.Camera(0), display=SimpleCV.Display()):
 	normaldisplay = 0
 	hull = False
 
@@ -108,27 +136,32 @@ def run(cam=SimpleCV.Camera(0)):
 					display_orientation(img, largest_square)
 
 					# Guidance 
-					vector_to_center(img, largest_square.centroid())
+					display_vector_to_center(img, largest_square.centroid())
 					check_position(img, largest_square)
 					# Use for convex hull instead of rectangle
 					if hull:
-						draw_hull(img, largest_square)
+						display_hull(img, largest_square)
 
 					else:
 						img.drawRectangle(x1, y1, width, height, SimpleCV.Color.BLUE, 3)
 
+					# Center of blob
 					img.drawCircle((largest_square.x,largest_square.y), 3, SimpleCV.Color.BLUE, 3)
-		img.drawCircle(CENTER, 3, SimpleCV.Color.BLUE, 3)
 		display_overlay(img, biggest_blob, normaldisplay, segmented_red)
 			
 
 if __name__ == "__main__":
-	if len(sys.argv) > 1:
-		if sys.argv[1] != "--ip":
-			print "System arguments: --ip [for web camera]"
-			exit(0)
-		address = "admin:admin@192.168.29.206:8081/video"
-		cam = SimpleCV.JpegStreamCamera(address)
-		run(cam)
-	else:
-		run()
+	display = SimpleCV.Display()
+	try:
+		if len(sys.argv) > 1:
+			if sys.argv[1] != "--ip":
+				print "System arguments: --ip [for web camera]"
+				exit(0)
+			address = "admin:admin@192.168.29.206:8081/video"
+			cam = SimpleCV.JpegStreamCamera(address)
+			print cam.getAllProperties()
+			run(cam, display)
+		else:
+			run(display=display)
+	except:
+		display.quit()

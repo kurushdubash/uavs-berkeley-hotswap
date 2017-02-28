73d59dc851cbe73a482f48f5a9a0d52ee07882ba
suneelbelkhale <neelus.genius@gmail.com>
2017-02-27 16:40:17 -0800

    refactoring and constants

--- hex/hex.ino
+++ hex/hex.ino
@@ -8,17 +8,16 @@
   This module is for the hexacopter landing pad.
 */
 
+// MOTOR SHIELD (1)
+Adafruit_MotorShield AFMS;
+
+
 // LEDs will be used as on-board indicators
 int LED_GREEN = 7; // Set green LED pin to 7
 int LED_RED = 5; // Set red LED pin to 5
 
 // Motor is used for screwing in our battery
-// initialize the stepper library on pins 8 through 11:
 int steps_per_rev = 200;
-
-Stepper stepper_motor(steps_per_rev, 0,1,2,3); //change for actual stepper
-
-Adafruit_MotorShield AFMS;
 Adafruit_StepperMotor *stepper;
 
 //directions and pwm's
@@ -27,12 +26,24 @@ int treadmill_dc_pwm = 0;
 int belt_dc_dir = 0;
 int belt_dc_pwm = 0;
 
-// Optional: Ultra-sonic sensor for triggering when drone is in position
+// Ultra-sonic sensor for triggering when drone is in position
 int trigPin = 11; // intialize trig Pin to 12 & echoPin to 11
 int echoPin = 12;
 
-//electromagnet
-int emPin = 8; // change port
+// electromagnet
+int emPin = 8; 
+
+//speed constants
+int STEPPER_UNSCREW_SPEED = 200;
+int BELT_MOVE_SPEED = 100;
+int TREADMILL_SPEED = 100;
+
+//time constants
+int LANDING_DELAY = 1000;
+int BELT_MOVE_TIME = 250;
+int TREADMILL_WAIT_TIME = 2000;
+int SCREW_DELAY = 100;
+int POST_SWAP_DELAY = 10000;
 
 QueueArray <double> distances;
 double SONAR_DIST_THRESH = 3.0;
@@ -44,7 +55,7 @@ void setup()
   AFMS = Adafruit_MotorShield();
   AFMS.begin();
   stepper = AFMS.getStepper(768, 1);
-  stepper->setSpeed(100);
+  stepper->setSpeed(STEPPER_UNSCREW_SPEED);
   
   Serial.print("Booting up...");
   // start the serial monitor
@@ -146,28 +157,29 @@ void performSwap(){
     digitalWrite(emPin, HIGH);
     Serial.println("ON.");
     Serial.print("Waiting for quad to land...");
-    while(!quadHasLanded());
+//    while(!quadHasLanded());
+    delay(LANDING_DELAY);
   /* Landing happens now -- DEFINITELY */
 
   /* Push in stepper motor with belt_dc */
     Serial.print("Pushing in stepper motor...");
     digitalWrite(belt_dc_dir, HIGH); //clockwise
-    analogWrite(belt_dc_pwm, 100); //in
-    delay(500);
+    analogWrite(belt_dc_pwm, BELT_MOVE_SPEED); //in
+    delay(BELT_MOVE_TIME);
     analogWrite(belt_dc_pwm, 0);
     Serial.println("PUSHED.");
   
   /* Stepper motor unscrews */
     Serial.print("Unscrewing...");
-    stepper_motor.step(-2*steps_per_rev);
-    delay(500);
+    stepper->step(2*steps_per_rev, BACKWARD, DOUBLE);
+    delay(SCREW_DELAY);
     Serial.println("UNSCREWED.");
       
   /* Push out stepper motor with belt_dc */
-    Serial.print("Turning on electro magnets...");
+    Serial.print("Pushing out stepper motor...");
     digitalWrite(belt_dc_dir, LOW); //counter clockwise
-    analogWrite(belt_dc_pwm, 100); //out
-    delay(500);
+    analogWrite(belt_dc_pwm, BELT_MOVE_SPEED); //out
+    delay(BELT_MOVE_TIME);
     analogWrite(belt_dc_pwm, 0);
     Serial.println("ON.");
 
@@ -176,30 +188,30 @@ void performSwap(){
   /* DC motor treadmill slide battery in */
     Serial.print("Sliding in battery...");
     digitalWrite(treadmill_dc_dir, HIGH);
-    analogWrite(treadmill_dc_pwm, 80); //moves treadmill
-    delay(1000);
+    analogWrite(treadmill_dc_pwm, TREADMILL_SPEED); //moves treadmill
+    delay(TREADMILL_WAIT_TIME);
     analogWrite(treadmill_dc_pwm, 0);
     Serial.println("IN.");
 
   /* Push in stepper motor with belt_dc */
     Serial.print("Push in stepper motor...");
     digitalWrite(belt_dc_dir, HIGH); //clockwise
-    analogWrite(belt_dc_pwm, 100);
-    delay(500);
+    analogWrite(belt_dc_pwm, BELT_MOVE_SPEED);
+    delay(BELT_MOVE_TIME);
     analogWrite(belt_dc_pwm, 0);
     Serial.println("PUSHED.");
 
   /* Stepper motor screws in */
     Serial.print("Screwing in...");
-    stepper_motor.step(2*steps_per_rev); //two rotations
-    delay(500);
+    stepper->step(2*steps_per_rev, FORWARD, DOUBLE); //two rotations
+    delay(SCREW_DELAY);
     Serial.println("IN.");
 
   /* Push out stepper motor with belt_dc */
     Serial.print("Pushing out stepper motor...");
     digitalWrite(belt_dc_dir, LOW); //counter-clockwise
-    analogWrite(belt_dc_pwm, 100);
-    delay(500);
+    analogWrite(belt_dc_pwm, BELT_MOVE_SPEED);
+    delay(BELT_MOVE_TIME);
     analogWrite(belt_dc_pwm, 0);
     Serial.println("OUT.");
 
@@ -207,7 +219,7 @@ void performSwap(){
     Serial.print("Turning off electro magnets...");
     digitalWrite(emPin, LOW);
     Serial.println("OFF.");
-    delay(10000); // Sleep 10 seconds to start our search
+    delay(POST_SWAP_DELAY); // Sleep 10 seconds to start our search
     Serial.print("Looking for quad..."); // Restart our loop
   /* Take off */
 }

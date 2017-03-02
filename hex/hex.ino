10e74b47f29ec123d4a8794a25a77b6be69fd3b5
suneelbelkhale <neelus.genius@gmail.com>
2017-03-02 00:30:59 -0800

    added correct dc motors

--- hex/hex.ino
+++ hex/hex.ino
@@ -9,7 +9,8 @@
 */
 
 // MOTOR SHIELD (1)
-Adafruit_MotorShield AFMS;
+Adafruit_MotorShield AFMS_bottom(0x60);
+Adafruit_MotorShield AFMS_top(0x61);
 
 
 // LEDs will be used as on-board indicators
@@ -19,12 +20,20 @@ int LED_RED = 5; // Set red LED pin to 5
 // Motor is used for screwing in our battery
 int steps_per_rev = 200;
 Adafruit_StepperMotor *stepper;
+Adafruit_DCMotor *treadmill_dc;
+Adafruit_DCMotor *belt_dc;
+Adafruit_DCMotor *battery_mover_dc;
 
 //directions and pwm's
 int treadmill_dc_dir = 0;
 int treadmill_dc_pwm = 0;
 int belt_dc_dir = 0;
 int belt_dc_pwm = 0;
+
+//initializing the speed at which each motor will run
+int treadmill_dc_speed = 100;
+int belt_dc_speed = 100;
+int battery_mover_dc_speed = 100;
 
 // Ultra-sonic sensor for triggering when drone is in position
 int trigPin = 11; // intialize trig Pin to 12 & echoPin to 11
@@ -40,9 +49,9 @@ int TREADMILL_SPEED = 100;
 
 //time constants
 int LANDING_DELAY = 1000;
-int BELT_MOVE_TIME = 250;
+int BELT_MOVE_TIME = 1000;
 int TREADMILL_WAIT_TIME = 2000;
-int SCREW_DELAY = 100;
+int SCREW_DELAY = 1000;
 int POST_SWAP_DELAY = 10000;
 
 QueueArray <double> distances;
@@ -52,10 +61,18 @@ void setup()
 {
   
   // intialize motors 
-  AFMS = Adafruit_MotorShield();
-  AFMS.begin();
-  stepper = AFMS.getStepper(768, 1);
+  AFMS_bottom.begin();
+  AFMS_top.begin();
+  stepper = AFMS_bottom.getStepper(768, 2);
   stepper->setSpeed(STEPPER_UNSCREW_SPEED);
+  treadmill_dc = AFMS_bottom.getMotor(1);
+  belt_dc = AFMS_bottom.getMotor(2);
+  battery_mover_dc = AFMS_top.getMotor(1);
+
+  //set motors to run at their corresponding speeds
+  treadmill_dc->setSpeed(treadmill_dc_speed);
+  belt_dc->setSpeed(belt_dc_speed);
+  battery_mover_dc->setSpeed(battery_mover_dc_speed);
   
   Serial.print("Booting up...");
   // start the serial monitor
@@ -65,11 +82,11 @@ void setup()
   pinMode(LED_GREEN, OUTPUT);
   pinMode(LED_RED, OUTPUT);
 
-  pinMode(belt_dc_dir, OUTPUT);
-  pinMode(treadmill_dc_dir, OUTPUT);
-  pinMode(belt_dc_pwm, OUTPUT);
-  pinMode(treadmill_dc_pwm, OUTPUT);
-  
+//  pinMode(belt_dc_dir, OUTPUT);
+//  pinMode(treadmill_dc_dir, OUTPUT);
+//  pinMode(belt_dc_pwm, OUTPUT);
+//  pinMode(treadmill_dc_pwm, OUTPUT);
+//  
   // initalize Ultra-sonic sensor
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
@@ -80,6 +97,8 @@ void setup()
   
   Serial.println("BOOT COMPLETE."); 
   Serial.print("Looking for quad...");
+
+//  performSwap();
 }
 
 
@@ -163,10 +182,12 @@ void performSwap(){
 
   /* Push in stepper motor with belt_dc */
     Serial.print("Pushing in stepper motor...");
-    digitalWrite(belt_dc_dir, HIGH); //clockwise
-    analogWrite(belt_dc_pwm, BELT_MOVE_SPEED); //in
+//    digitalWrite(belt_dc_dir, HIGH); //clockwise
+//    analogWrite(belt_dc_pwm, BELT_MOVE_SPEED); //in
+    belt_dc->run(FORWARD);
     delay(BELT_MOVE_TIME);
-    analogWrite(belt_dc_pwm, 0);
+//    analogWrite(belt_dc_pwm, 0);
+    belt_dc->run(RELEASE);
     Serial.println("PUSHED.");
   
   /* Stepper motor unscrews */
@@ -177,28 +198,34 @@ void performSwap(){
       
   /* Push out stepper motor with belt_dc */
     Serial.print("Pushing out stepper motor...");
-    digitalWrite(belt_dc_dir, LOW); //counter clockwise
-    analogWrite(belt_dc_pwm, BELT_MOVE_SPEED); //out
+//    digitalWrite(belt_dc_dir, LOW); //counter clockwise
+//    analogWrite(belt_dc_pwm, BELT_MOVE_SPEED); //out
+    belt_dc->run(BACKWARD);
     delay(BELT_MOVE_TIME);
-    analogWrite(belt_dc_pwm, 0);
+//    analogWrite(belt_dc_pwm, 0);
+    belt_dc->run(RELEASE);
     Serial.println("ON.");
 
   /* Magazine loads new pack on treadmill */
   
   /* DC motor treadmill slide battery in */
     Serial.print("Sliding in battery...");
-    digitalWrite(treadmill_dc_dir, HIGH);
-    analogWrite(treadmill_dc_pwm, TREADMILL_SPEED); //moves treadmill
+//    digitalWrite(treadmill_dc_dir, HIGH);
+//    analogWrite(treadmill_dc_pwm, TREADMILL_SPEED); //moves treadmill
+    treadmill_dc->run(FORWARD);
     delay(TREADMILL_WAIT_TIME);
-    analogWrite(treadmill_dc_pwm, 0);
+//    analogWrite(treadmill_dc_pwm, 0);
+    treadmill_dc->run(RELEASE);
     Serial.println("IN.");
 
   /* Push in stepper motor with belt_dc */
     Serial.print("Push in stepper motor...");
-    digitalWrite(belt_dc_dir, HIGH); //clockwise
-    analogWrite(belt_dc_pwm, BELT_MOVE_SPEED);
+//    digitalWrite(belt_dc_dir, HIGH); //clockwise
+//    analogWrite(belt_dc_pwm, BELT_MOVE_SPEED);
+    belt_dc->run(FORWARD);
     delay(BELT_MOVE_TIME);
-    analogWrite(belt_dc_pwm, 0);
+//    analogWrite(belt_dc_pwm, 0);
+    belt_dc->run(RELEASE);
     Serial.println("PUSHED.");
 
   /* Stepper motor screws in */
@@ -209,15 +236,22 @@ void performSwap(){
 
   /* Push out stepper motor with belt_dc */
     Serial.print("Pushing out stepper motor...");
-    digitalWrite(belt_dc_dir, LOW); //counter-clockwise
-    analogWrite(belt_dc_pwm, BELT_MOVE_SPEED);
+//    digitalWrite(belt_dc_dir, LOW); //counter-clockwise
+//    analogWrite(belt_dc_pwm, BELT_MOVE_SPEED);
+    belt_dc->run(BACKWARD);
     delay(BELT_MOVE_TIME);
-    analogWrite(belt_dc_pwm, 0);
+//    analogWrite(belt_dc_pwm, 0);
+    belt_dc->run(RELEASE);
     Serial.println("OUT.");
 
   /* Electromagnets disengage */
     Serial.print("Turning off electro magnets...");
     digitalWrite(emPin, LOW);
+
+    battery_mover_dc->run(FORWARD);
+    delay(1000);
+    battery_mover_dc->run(RELEASE);
+    
     Serial.println("OFF.");
     delay(POST_SWAP_DELAY); // Sleep 10 seconds to start our search
     Serial.print("Looking for quad..."); // Restart our loop
